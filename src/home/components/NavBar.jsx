a30a8937f0c092f237eddcd2542a36854a16deed
Hiamoutgame <anhvietanh1123@gmail.com>
2025-09-30 21:48:55 +0700

    feat update logo to center in homepage

--- src/home/components/NavBar.jsx
+++ src/home/components/NavBar.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import "./NavBar.css";
-
+import logo from "./../../assets/logo.png";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">EV Swap</div>
       <ul className="navbar-links">
-        <li><a href="#">Trang chủ</a></li>
-        <li><a href="#">Giới thiệu</a></li>
-        <li><a href="#">Gói dịch vụ</a></li>
-        <li><a href="#">Trạm gần nhất</a></li>
-        <li><a href="#">Liên hệ</a></li>
+        <li>
+          <a href="#">Trang chủ</a>
+        </li>
+        <li>
+          <a href="#">Giới thiệu</a>
+        </li>
+        <li>
+          <a href="#">Gói dịch vụ</a>
+        </li>
+        <li>
+          <a href="#">Trạm gần nhất</a>
+        </li>
+        <li>
+          <a href="#">Liên hệ</a>
+        </li>
       </ul>
+      <div className="navbar-logo">
+        <img src={logo} alt="" />
+      </div>
     </nav>
   );
 };
