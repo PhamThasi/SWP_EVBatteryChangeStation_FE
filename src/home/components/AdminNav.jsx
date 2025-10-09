c87bab0ac2da4d4987331cd17c7d095229908ebd
nguyentgse141092 <nguyentgse141092@fpt.edu.vn>
2025-10-09 09:54:09 +0700

    edit admin nav bar

--- src/home/components/AdminNav.jsx
+++ src/home/components/AdminNav.jsx
@@ -6,7 +6,7 @@ const AdminNav = () => {
     <nav className="navbar">
       <div className="navbar-logo">EV Swap</div>
       <ul className="navbar-links">
-        <li><a href="#">Trang chủ</a></li>
+        <li><a href="#">Quan ly tai khoan</a></li>
         <li><a href="#">Thong ke</a></li>
         <li><a href="#">Kho hang</a></li>
         <li><a href="#">Danh sach khach hang</a></li>
